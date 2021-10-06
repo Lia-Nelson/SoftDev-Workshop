944b248d49ecd741190bbbc9d66ae36c3ae508ca
Julia Nelson <90808935+Lia-Nelson@users.noreply.github.com>
2021-10-05 21:47:27 -0400

    Update app.py

--- 10_flask/occupy_flask_st/app.py
+++ 10_flask/occupy_flask_st/app.py
@@ -3,38 +3,31 @@
 # K10 -- Return a random professional field based on the percentage of jobs in that field
 # 2021-10-06
 
-# imports csv and random as we will be using functions from those modules
-import csv
-import random
 from flask import Flask
-app = Flask(__name__)
+import random
+import csv
+app = Flask(__name__) #create instance of class Flask
 
-# creates a function called openCSV which returns a dictionary where the value is a float
-# representing the percent change of being chosen and the total is the total of those
-# values (since they may not add up to exactly 100)
 def openCSV(fname):
     # creates an empty dictionary and sets the temp_total and total to 0
     dict = {}
     temp_total = 0
     total = 0
-    # opens the files and gives it the name f
     with open(fname) as f:
-        # reads the first line (where the title is) so that it will not impact the dict
+        # ensures that the column headers are not included in the dictionary 
         f.readline()
-        # creates a csv reader of f, indicates that a comma is separation
         reader = csv.reader(f, delimiter=',')
         for row in reader:
-            # if the row is contians 'Total', instead of being saved to the dict, its
-            # float is saved as total
+            # separately saves the total of the probabilities for each occupation
             if 'Total' in row:
                 total = float(row[1])
             else:
-                # creates a new row at the start of the dict with the first element
-                # read in by the csv as the key and the second as the value
+                # populates dictionary 'dict' with (key: value) pairs that represent
+                #  occupations and their probabilities
                 dict[row[0]] = float(row[1])
                 # keeps track of consecutive sum of the values
                 temp_total += float(row[1])
-        # if a total is not given, sets it to the consecutive some of the value
+        # if a total is not given, sets it to the sum of the value
         if total == 0:
             total = temp_total
         return dict, total
@@ -42,73 +35,70 @@ def openCSV(fname):
 # picks an occupation based on the weighted percentages
 def picker():
     jobDict, total = openCSV('occupations.csv')
-    # we multiply the total by 10 so that it is represented as an integer, since the
-    # randint function will produce an integer
+    # we multiply the total by 10 for random.randint() to produce an int to 
+    #  simulate randomly selecting an occupation
     total = total * 10
-    # creates a list of the perfecntages and occupations (indexed similarly)
+    # stores a list of occupations and their corresponding probabilities in 
+    #  two separate lists
     nums = list(jobDict.values())
-    # print(sum(nums))
     occ = list(jobDict.keys())
     conDict = {}
     sum = 0
-    # the key for the index is set as the occupation, and the value as ten times the
-    # consecutive sum (so that it can be used for randint)
+    # populates conDict such that each (key: value) pair represents the occupation
+    #  and 10 times the sum of the probabilities preceding that occupation
     for i in range(len(nums)):
         nums[i] *= 10.0
         sum += nums[i]
         nums[i] = sum
     for i in range(len(occ)):
         conDict[occ[i]] = nums[i]
-    # print(jobDict)
-    # print(conDict)
 
-    # for i in occ:
-    # picks a random int from 0 inclusive to total exclusive so that the number of
-    # integers picked equals the possibility for probabilities
+    # picks a random int from 0 inclusive to the total exclusive so that the number of
+    #  integers picked equals the possibility for probabilities
     n = random.randint(0, total-1)
-    # for occupations in conDict if the randomly selected integer is less than the
-    # consecutive probability it will be returned
+    # returns the occupation if its probability is within range of the consecutive sum
     for i in conDict:
         if (n < conDict[i]):
             return i
 
-# Picks random occupations repeatedly until it reaches n trials then checks to see
-# if the outcomes of what was picked match their probabilities, will not be run
-# in final code
+# runs n trials to determine if the probability of the resulting occupations matches 
+#  those listed in the .csv file
 def test_probs(n):
-    # reads the csv file and stores its data in a dictionary using prebuilt function, stores total percent
     testResults, total = openCSV('occupations.csv')
-    # loop through keys and set their counters to 0
     for i in testResults:
         testResults[i] = 0
-    # run n trials - each time key (a profession) is returned increment its value in the dictionary by 1
     for i in range(n):
         testResults[picker()] += 1
-    # loop through dictionary and convert value to a list with the number of times returned and a percentage
     for i in testResults:
         testResults[i] = [testResults[i], round(testResults[i] / n * 10000) / 100]
     print(testResults)
 
 def getList():
-    dict, total = openCSV('occupations.csv')
-    text = '''<ul>'''
-    for i in dict:
-        text += f"<li>{i}</li>"
-    text += "</ul>"
-    return text
-
-
-@app.route("/")
-def hello_world():
-    occupations = getList()
-    html = f'''
-    Goofy Goobers - Oscar Wang, Owen Yaggy, Julia Nelson
-    <br><b>Your randomized output:</b> {picker()}
-    <br><br>List of all applications:
-    {getList()}
+    testResults, total = openCSV('occupations.csv')
+    HTMLList = '''
+    <ul>
     '''
-    print("Displayed random output successfully")
-    return html
+    occupations = testResults.keys()
+    for i in occupations:
+        HTMLList += f"<li>{i}</li>\n"
+    HTMLList += "</ul>"
+    return HTMLList
 
 
+@app.route("/")       #assign fxn to route
+def hello_world():
+    output = f'''
+    <head>
+    <h1>Goofy Goobers: Julia Nelson, Oscar Wang, Owen Yaggy</h1>
+    <p>Oct 2021</p>
+    <p>K10: Putting Little Pieces Together</p>
+    </head>
+    <body>
+    <b>Your randomized output: </b> {picker()} {getList()}
+    </body>
+    '''
+    print(__name__)   #where will this go?
+    return output
+
+app.debug = True
 app.run()
