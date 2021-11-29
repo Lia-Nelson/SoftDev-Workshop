a1649497875b1e7613837c1d1b43c60537962900
Julia Nelson <90808935+Lia-Nelson@users.noreply.github.com>
2021-11-29 08:04:43 -0500

    Update app.py

--- 19_api/app.py
+++ 19_api/app.py
@@ -6,28 +6,28 @@ K19 -- A RESTful Journey Skyward
 time spent: 2.3 hours
 """
   
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import json
-import urllib.request
+import urllib
 
 app = Flask(__name__)
 
-def display_Info(storage):
-    explanation = storage.data[explanation]
-    url = storage.data[url]
+def display_Info(ssstorage):
+    explanation = ssstorage['explanation']
+    url = ssstorage['url']
     return render_template("main.html", explanation, url)
 
 @app.route("/")
 def main():
-    nasa = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=BbBUhlazreBfN6DQvmGnAp6TcuBzsfUQJISPj1hK")
+    url = "https://api.nasa.gov/planetary/apod?api_key="
+    url += str(open("key_nasa.txt", "r").read())
+    nasa = urllib.request.urlopen(url)
     library_of_info = json.loads(nasa.read())
     display_Info(library_of_info)
     # print(library_of_info)
 
 if __name__ == "__main__":
-    """
-    false if this file imported as module
-    debugging enabled
-    """
+    # false if this file imported as module
+    # debugging enabled
     app.debug = True
     app.run()
