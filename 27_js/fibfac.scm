52a833f699d2394b1396600e2a47a2994f318bc9
Lia-Nelson <jnelson20@stuy.edu>
2022-02-04 08:27:40 -0500

    27_js created, k17 renamed to 17

---
+++ 27_js/fibfac.scm
@@ -0,0 +1,30 @@
+(define fact
+  (lambda (n)
+    (if (= n 1)
+        1
+        (* n (fact (- n 1)))
+        )
+    )
+  )
+
+(fact 1)
+(fact 2)
+(fact 3)
+(fact 4)
+
+(define fib
+  (lambda (n)
+    (if (<= n 1)
+        n
+        (+ (fib (- n 1)) (fib (- n 2)))
+        )
+    )
+  )
+
+(fib -1)
+(fib 0)
+(fib 1)
+(fib 2)
+(fib 3)
+(fib 4)
+(fib 5)
