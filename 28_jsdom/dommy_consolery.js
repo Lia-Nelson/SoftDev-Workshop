5657d39f8aef2c96a2cbd7238d69b742f28d312d
Lia-Nelson <jnelson20@stuy.edu>
2022-02-08 21:35:34 -0500

    index.html and notes.txt updated

--- 28_jsdom/dommy_consolery.js
+++ 28_jsdom/dommy_consolery.js
@@ -68,6 +68,9 @@ var f = function(x) {
 
 
 //instantiate an object
+
+//typing o into the console returns the object with a list
+//of its properties
 var o = { 'name' : 'Thluffy',
           age : 15,
           items : [10, 20, 30, 40],
@@ -77,7 +80,25 @@ var o = { 'name' : 'Thluffy',
           }
         };
 
+//typing addItem will display the function with a list of
+//its properties
 
+//typing addItem() will result in undefined being added to
+//the list of items (inner HTML)
+
+//typing addItem(a) where a is an undefined variable returns
+//a reference error
+
+//typing addItem(n) where n is a defined variable (not) a function
+//returns undefined and leads to the value of the variable (no quotes)
+//being added to the list
+
+//typing addItem(o) returns undefined and causes [object Object] to be
+//added to the list
+
+//typing addItem(f) returns undefiend and causes the function and its
+//definition (e.g. function(x) { var j=30; return j+x; }) to be added
+//to the list
 var addItem = function(text) {
   var list = document.getElementById("thelist");
   var newitem = document.createElement("li");
@@ -85,13 +106,29 @@ var addItem = function(text) {
   list.appendChild(newitem);
 };
 
+//typing removeItem will display the function with a list of
+//its properties
 
+//typing removeItem() or the function without one parameter will
+//return an error
+
+//typing removeItem() with any parameter that is not a whole number within
+//the indicies of the list of items in the inner HTML or a variable does
+//not represent such a whole number (i.e. j) will return an error
+
+//typing removeItem(n) where n is a whole number within the indices of the
+//appropriate list will remove the element at that index
 var removeItem = function(n) {
   var listitems = document.getElementsByTagName('li');
   listitems[n].remove();
 };
 
+//typing red will display the function with a list of
+//its properties
 
+//typing red() adds red to the beginning of the class list for
+//all elements, changing the color only of elements that do not have
+//a color class
 var red = function() {
   var items = document.getElementsByTagName("li");
   for(var i = 0; i < items.length; i++) {
@@ -99,7 +136,12 @@ var red = function() {
   }
 };
 
-
+//typing stripe will display the function with a list of
+//its properties
+//typing stripe will change the color of the list items in the
+//appropriate location to either red or blue (consecutively) if
+//they do not already have a color class (since the class is added
+//to the beginning of the class list)
 var stripe = function() {
   var items = document.getElementsByTagName("li");
   for(var i = 0; i < items.length; i++) {
@@ -113,15 +155,42 @@ var stripe = function() {
 
 //insert your implementations here for...
 // FIB
+//returns an error if any inputs undefined variables
+//returns undefined if the input is a decimal
+//otherwise returns one if the input is less than or equal to 1
+//for positive integers returns the appropriate fibonacci number
+//variables corresponding to numbers also work as valid inputs
 var fib = (n) => {
+  if ((n % 1) != 0) return undefined;
   if (n <= 1) return n;
   else return fib(n - 1) + fib(n - 2);
 };
 
 // FAC
+//returns an error if any inputs undefined variables
+//returns undefined if the input is a decimal
+//otherwise returns one if the input is less than or equal to 1
+//for positive integers returns the value of its factorial
+//variables corresponding to numbers also work as valid inputs
 var fact = function(n) {
+  if ((n % 1) != 0) return undefined;
   if (n <= 1) return 1;
   else return n * fact(n - 1);
 };
 
 // GCD
+//returns an error if any inputs undefined variables
+//returns undefined if either of the inputs are not integers
+//else returns a positive integer corresponding (the
+//greatest common denominator of the inputs)
+//variables corresponding to numbers also work as valid inputs
+var gcd = function(a, b) {
+  if ((a % 1) != 0) return undefined;
+  if ((b % 1) != 0) return undefined;
+  if (b == 0) return Math.abs(a);
+  else return gcd(b, (a % b));
+};
+
+addItem("fib(2): " + fib(2));
+addItem("fact(5): " + fact(5));
+addItem("gcd(22, 11): " + gcd(22, 11));
