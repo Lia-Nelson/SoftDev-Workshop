2f1f7b7550526a7667e89fa53e8cf565be2f543e
Lia-Nelson <jnelson20@stuy.edu>
2022-02-15 21:17:39 -0500

    K30 improved

--- 30_jsdraw/clickdraw.js
+++ 30_jsdraw/clickdraw.js
@@ -16,37 +16,62 @@ var mode = "rect";
 var toggleMode = (e) => {
     console.log("original mode: " + mode);
     if(mode == "rect"){
-        mode = "circ"
+        mode = "circ";
+        bToggler.innerHTML = "Circle";
     }
     else if (mode == "circ"){
         mode = "rect";
+        bToggler.innerHTML = "Rectangle";
     }
     else{
         console.log("houston, we've got a problem");
     }
 
-}
+};
 
 var drawRect = function(e){
+  console.log("making rect");
   ctx.beginPath();
-  let x = e.offSetX;
-  let y = e.offSetY;
+  let x = e.offsetX;
+  console.log("x: " + x);
+  let y = e.offsetY;
+  console.log("y: " + y);
   ctx.strokeStyle = 'red';
   ctx.fillStyle = 'red';
-  ctx.fillRect(x, y, 5, 10);
-}
+  console.log(ctx);
+  ctx.fillRect(x, y, 50, 100);
+};
 
 var drawCircle = function(e){
-    //////
-}
+  console.log("making circ");
+  ctx.beginPath();
+  let x = e.offsetX;
+  console.log("x: " + x);
+  let y = e.offsetY;
+  console.log("y: " + y);
+  ctx.arc(x, y, 25, 0, 360);
+  ctx.strokeStyle = 'black';
+  ctx.fillStyle = 'red';
+  console.log(ctx);
+  ctx.fill();
+  ctx.stroke();
+};
 
 var draw = function(e){
-    ///////
-}
+  if (mode === "rect") {
+      drawRect(e);
+  } else {
+      drawCircle(e);
+  }
+};
 
 var wipeCanvas = () => {
-    /////
-}
+  console.log("wiping canvas...");
+  ctx.clearRect(0, 0, c.clientWidth, c.clientHeight);
+};
 
-c.addEventListener('click',draw);
-var bToggler
+c.addEventListener("click", draw);
+let bToggler = document.getElementById("buttonToggle");
+bToggler.addEventListener("click", toggleMode);
+let clearB = document.getElementById("buttonClear");
+clearB.addEventListener("click", wipeCanvas);
